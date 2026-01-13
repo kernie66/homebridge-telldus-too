45dcaedc8803f70713363e4efd58e192e4c4aa43
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2026-01-13 20:36:45 +0100

    Added tests for checkStatusCode

---
+++ src/utils/__tests__/checkStatusCode.test.ts
@@ -0,0 +1,47 @@
+import { describe, it } from 'vitest';
+import checkStatusCode from '../checkStatusCode';
+
+describe('Test Telldus API status code checker', () => {
+  it('checks a normal 200 response', () => {
+    const response = {
+      statusCode: 200,
+      statusMessage: 'OK',
+      body: {
+        reply: 'success',
+      },
+    };
+
+    const checkedResponse = checkStatusCode(response);
+    expect(checkedResponse).toBeFalsy();
+  });
+
+  it('checks a 200 response with error in body', () => {
+    const response = {
+      statusCode: 200,
+      statusMessage: 'OK',
+      body: {
+        error: 'Some error occurred',
+      },
+    };
+    const logger = (message: string) => {
+      console.log('Logger message:', message);
+      expect(message).toBe('Telldus replies with error:');
+    };
+    const checkedResponse = checkStatusCode(response, logger);
+    expect(checkedResponse).toBeTruthy();
+  });
+
+  it('checks a 401 Unauthorized response', () => {
+    const response = {
+      statusCode: 401,
+      statusMessage: 'Unauthorized',
+      body: {},
+    };
+    const logger = (message: string) => {
+      console.log('Logger message:', message);
+      expect(message).toBe('Access denied, check if the access token is valid');
+    };
+    const checkedResponse = checkStatusCode(response, logger);
+    expect(checkedResponse).toBeTruthy();
+  });
+});
